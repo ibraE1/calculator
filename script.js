e804d7c8c5c74fa0092f75f26709ab6ef6c331cd
ibraE1 <ibrahimelwany@hotmail.com>
2021-08-21 17:32:12 +0200

    functional display

--- script.js
+++ script.js
@@ -26,3 +26,14 @@ function operate(operator, a, b) {
       return divide(a, b);
   }
 }
+
+const display = document.querySelector("#display");
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => button.addEventListener("click", () => {
+    const input = document.createElement("h1");
+    input.textContent += button.textContent;
+    input.classList.add("input");
+    display.appendChild(input);
+}));
